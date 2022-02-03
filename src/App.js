fb7db04eb60de5abeae8ecc9cb5e0cda582c8094
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2022-02-03 10:23:10 -0800

    adds x and y axis

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
-
 import './App.css';
-import React, { useRef, useEffect, useState }from "react";
-// import * as d3 from 'd3';
-import { select } from "d3";
-
-// const data = [35, 30, 45, 60, 30];
+import React, { useRef, useEffect, useState } from "react";
+import { select, line, curveCardinal, axisBottom, axisRight, scaleLinear } from "d3";
 
 function App() {
-  const [data, setData] = useState([35, 30, 45, 60, 30]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef();
+
+  // will be called initially and on every data change
   useEffect(() => {
-    console.log(svgRef);
-    const svg = select(svgRef.current)
-    svg.selectAll("circle")
-    .data(data)
-    .join(
-      enter => enter.append("circle"),
-      update => update.attr("class", "updated"),
-      exit => exit.remove() 
-    );
-  },[data]);
+    const svg = select(svgRef.current);
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+
+    const yScale = scaleLinear()
+      .domain([0, 150])
+      .range([150, 0]);
+
+    const xAxis = axisBottom(xScale);
+    svg
+      .select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg
+      .select(".y-axis")
+      .style("transform", "translateX(300px)")
+      .call(yAxis);
+  
+    //generates the "d" attribute of a path element
+    const myLine = line()
+      .x((value, index) => xScale(index))
+      .y(yScale)
+      .curve(curveCardinal);
+
+    //renders path element, and attaches
+    // the "d" attribute from the line generator above
+    svg
+      .selectAll(".line")
+      .data([data])
+      .join("path")
+      .attr("class", "line")
+      .attr("d", myLine)
+      .attr("fill", "none")
+      .attr("stroke", "blue");
+  }, [data]);
+
   return (
     <React.Fragment>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
       <br />
       <button onClick={() => setData(data.map(value => value + 5))}>
         Update data
+      </button>
+      <button onClick={() => setData(data.map(value => value < 35))}>
+        Filter data
       </button>
     </React.Fragment>
   );
