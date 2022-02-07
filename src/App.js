773ae597a683b0a639e4938ee4f44d92dd21a1f6
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2022-02-07 13:09:37 -0800

    adds animation indicating height above each column.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,40 @@
-import './App.css';
 import React, { useRef, useEffect, useState } from "react";
-import { 
-  select, 
-  axisBottom, 
-  axisRight, 
-  scaleLinear,
-  scaleBand 
-} from "d3";
+import "./App.css";
+import { select, axisBottom, axisRight, scaleLinear, scaleBand } from "d3";
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
+  const [data, setData] = useState([25, 30, 45, 60, 10, 65, 75]);
   const svgRef = useRef();
 
   // will be called initially and on every data change
   useEffect(() => {
-    const svg = select(svgRef.current); 
+    const svg = select(svgRef.current);
+
+    // scales
     const xScale = scaleBand()
       .domain(data.map((value, index) => index))
       .range([0, 300])
       .padding(0.5);
 
-    const yScale = scaleLinear()
-      .domain([0, 150])
-      .range([150, 0]);
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
 
     const colorScale = scaleLinear()
-      .domain([50, 100, 150])
-      .range(["blue", "purple", "red"])
+      .domain([75, 100, 150])
+      .range(["green", "orange", "red"])
       .clamp(true);
 
-    const xAxis = axisBottom(xScale)
-      .ticks(data.length);
-    svg
-      .select(".x-axis")
-      .style("transform", "translateY(150px)")
-      .call(xAxis);
+    // create x-axis
+    const xAxis = axisBottom(xScale).ticks(data.length);
+    svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
 
+    // create y-axis
     const yAxis = axisRight(yScale);
     svg
       .select(".y-axis")
       .style("transform", "translateX(300px)")
       .call(yAxis);
-  
-    //generates the "d" attribute of a path element
+
+    // draw the bars
     svg
       .selectAll(".bar")
       .data(data)
@@ -52,11 +44,26 @@ function App() {
       .attr("x", (value, index) => xScale(index))
       .attr("y", -150)
       .attr("width", xScale.bandwidth())
+      .on("mouseenter", (event, value) => {
+        // events have changed in d3 v6:
+        // https://observablehq.com/@d3/d3v6-migration-guide#events
+        const index = svg.selectAll(".bar").nodes().indexOf(event.target);
+        svg
+          .selectAll(".tooltip")
+          .data([value])
+          .join((enter) => enter.append("text").attr("y", yScale(value) - 4))
+          .attr("class", "tooltip")
+          .text(value)
+          .attr("x", xScale(index) + xScale.bandwidth() / 2)
+          .attr("text-anchor", "middle")
+          .transition()
+          .attr("y", yScale(value) - 8)
+          .attr("opacity", 1);
+      })
+      .on("mouseleave", () => svg.select(".tooltip").remove())
       .transition()
       .attr("fill", colorScale)
-      .attr("height", value => 150 - yScale(value))
-      ;
-  
+      .attr("height", (value) => 150 - yScale(value));
   }, [data]);
 
   return (
@@ -65,20 +72,19 @@ function App() {
         <g className="x-axis" />
         <g className="y-axis" />
       </svg>
-      <br />
-      <button onClick={() => setData(data.map(value => value + 5))}>
+      <button onClick={() => setData(data.map((value) => value + 5))}>
         Update data
       </button>
-      <button onClick={() => setData(data.map(value => value < 35))}>
+      <button onClick={() => setData(data.filter((value) => value < 35))}>
         Filter data
+      </button>
+      <button
+        onClick={() => setData([...data, Math.round(Math.random() * 100)])}
+      >
+        Add data
       </button>
     </React.Fragment>
   );
 }
 
 export default App;
-
-////Loose bits////
-
-/* <div className="App">
-<svg width = "960" height = "500"></svg></div>   */
